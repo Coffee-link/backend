bc6298ccc9b84b493183b5361e6feeed3cce6ffc
WenheLI <wl1508@nyu.edu>
2021-06-12 14:16:51 +0800

    add db manager

--- index.py
+++ index.py
@@ -1,71 +1,97 @@
-from flask import Flask
-from flask import request
+# from flask import Flask
+# from flask import request
+from tinydb import TinyDB, Query
+from tinydb import operations
+from tinydb import Query
 
-class Profile:
-    def __init__(self, username, uuid, content, location):
-        self.username = username
-        self.uuid = uuid
-        self.content = content
-        self.location = location
-        self.histories=[]
+db = TinyDB('./db.json')
 
-    def to_json(self):
+class ProfileManager:
+    def __init__(self):
+        self.profiles = db.table('profiles')
+        self.Profile = Query()
+
+    def add_profiles(self, username, uuid, content, location):
+        results = self.get(uuid)
+        if (len(results) == 0):
+            profile = {
+                "username": username,
+                "uuid": uuid,
+                "content": content,
+                "location": location,
+                "histories": []
+            }
+            self.profiles.insert(self.to_json(profile))
+            return True
+        else:
+            return False
+
+    def to_json(self, profile):
         return {
-            "username": self.username,
-            "id": self.uuid,
-            "content": self.content,
-            "location": self.location
+            "username": profile.get('username', ''),
+            "id": profile.get('uuid', ''),
+            "content": profile.get('content', ''),
+            "location": profile.get('location', ''),
+            "histories": profile.get('histories')
         }
     
     def add_meeting(self, meeting):
         self.histories.append(meeting)
 
-    # def update(self, username=self.username, uuid=self.uuid, content=content, location=location):
-    #     self.username = username
-    #     self.uuid = uuid
-    #     self.content = content
-    #     self.location = location
+    def get(self, id):
+        return self.profiles.search(self.Profile.id == id)
+    
+    def delete(self, id):
+        profiles = self.profiles.search(self.Profile.id == id)
+        self.profiles.remove(doc_ids=[profile.doc_id for profile in profiles])        
 
-profiles = []
+    def update(self, id, data):
+        for k in data.keys():
+            self.profiles.update({k: data[k]}, self.Profile.id == id)
 
+m = ProfileManager()
+m.add_profiles('1', '2', '3', '4')
+print(m.get('2'))
+m.update('2', {
+    'location': '1234'
+})
+print(m.get('2'))
+# app = Flask(__name__)
 
+# @app.route('/')
+# def hello():
+#     # show the user profile for that user
+#     return 'hello'
 
-app = Flask(__name__)
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return {
+#         "user": username
+#     }
 
-@app.route('/')
-def hello():
-    # show the user profile for that user
-    return 'hello'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return {
-        "user": username
-    }
-
-@app.route('/profile', methods=["POST", "GET"])
-def add():
-    if request.method == 'POST':
-        raw_profile = request.json
-        profile = Profile(raw_profile)
-        profiles.append(profile)
-        return {
-            'status': 1,
-            'uuid': profile.uuid
-        }
-        # profile = Profile(username, uuid, content, location)
-        # profiles.append(profile)
-    elif request.method == 'GET':
-        raw_profile = request.json
-        id = raw_profile.id
-        print('get profile')
-        return 'get profile'
-        # return profile
-    elif request.method == 'DELETE':
-        print('DELETE profile')
-        return 'DELETE profile'
-        # profiles.remove(profile)
-    elif request.method == 'PUT':
-        return 'hiiii'
-        # profile.update(username, uuid, content, location)
+# @app.route('/profile', methods=["POST", "GET", "DELETE", "PUT"])
+# def add():
+#     if request.method == 'POST':
+#         raw_profile = request.json
+#         profile = Profile(raw_profile)
+#         profiles.append(profile)
+#         return {
+#             'status': 1,
+#             'uuid': profile.uuid
+#         }
+#         # profile = Profile(username, uuid, content, location)
+#         # profiles.append(profile)
+#     elif request.method == 'GET':
+#         raw_profile = request.json
+#         id = raw_profile.id
+#         print('get profile')
+#         return 'get profile'
+#         # return profile
+#     elif request.method == 'DELETE':
+#         print('DELETE profile')
+#         return 'DELETE profile'
+#         # profiles.remove(profile)
+#     elif request.method == 'PUT':
+#         return 'hiiii'
+#         # profile.update(username, uuid, content, location)
